9f84ab062f3e1e46b67149d7de8e2f71633c9d74
Andrey Moujikov <a@moujikov.email>
2026-07-28 21:13:04 +0300

    numpy no longer allows uneven-sized arrays – fixing it by using list() where necessary

--- craft_text_detector/craft_utils.py
+++ craft_text_detector/craft_utils.py
@@ -412,7 +412,7 @@ def getDetBoxes(textmap, linkmap, text_threshold, link_threshold, low_text, poly
 
 def adjustResultCoordinates(polys, ratio_w, ratio_h, ratio_net=2):
     if len(polys) > 0:
-        polys = np.array(polys)
+        polys = [np.array(poly) if poly is not None else None for poly in polys]
         for k in range(len(polys)):
             if polys[k] is not None:
                 polys[k] *= (ratio_w * ratio_net, ratio_h * ratio_net)
